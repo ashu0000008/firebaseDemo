e9692d2e6d00ece5eb0372b9df10301330be8bb6
peter <ashu0000008@163.com>
2023-08-21 16:32:41 +0800

    集成firebase crashlytics

--- app/src/main/java/com/example/firebase/MainActivity.kt
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.example.firebase
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.firebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -29,8 +28,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            throw RuntimeException("Test Crash")
         }
     }
 
